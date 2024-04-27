19392c5a133ced58e502a67b6fa7d9613a8a31c3
tiagomonteiroo <167870432+tiagomonteiroo@users.noreply.github.com>
2024-04-27 13:12:21 +0100

    Estabelecidas as ligações entre as views da app e os templates(HTML'S)

--- siteclube/clubehome/views.py
+++ siteclube/clubehome/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def homepage(request):
+    return render(request, "clubehome/homepage.html")
+
+def loja(request):
+    return render(request, "clubehome/loja.html")
+
+def plantel(request):
+    return render(request, "clubehome/plantel.html")
