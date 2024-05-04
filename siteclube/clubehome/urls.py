5c756e280570b2ec6532d79ff22673edd9506b65
jxbarbosax <jxbarbosax@gmail.com>
2024-05-04 16:29:31 +0100

    adicionei uns quantos q faltavam à homepage

--- siteclube/clubehome/urls.py
+++ siteclube/clubehome/urls.py
@@ -3,6 +3,11 @@ from . import views
 
 urlpatterns = [
     path("", views.homepage, name = "homepage"),
-    path("plantel/", views.plantel, name = "plantel" ),
-    path ("loja/", views.loja, name = "loja")
+    path("plantel/", views.plantel, name = "plantel"),
+    path("loja/", views.loja, name = "loja"),
+    path("bilhetes/", views.bilhetes, name = "bilhetes"),
+    path("noticias/", views.noticias, name = "noticias"),
+    path("resultados/", views.resultados, name = "resultados"),
+    path("clube/", views.clube, name = "clube"),
+    path("socio/", views.socio, name = "socio")
 ]
