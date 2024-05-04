8520739969ac5260be871a480da03e50910db26b
tiagomonteiroo <167870432+tiagomonteiroo@users.noreply.github.com>
2024-05-04 18:52:28 +0100

    Adicionar o Login

--- siteclube/siteclube/settings.py
+++ siteclube/siteclube/settings.py
@@ -116,9 +116,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'clubehome/static')
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'clubehome/static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
