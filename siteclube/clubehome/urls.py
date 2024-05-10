3d73e84011196cd82177c156d2531f71de5d7ff3
tiagomonteiroo <167870432+tiagomonteiroo@users.noreply.github.com>
2024-05-10 11:16:08 +0100

    BASE DE DADOS FINE

--- siteclube/clubehome/urls.py
+++ siteclube/clubehome/urls.py
@@ -4,14 +4,15 @@ from . import views
 urlpatterns = [
     path("", views.homepage, name = "homepage"),
     path("login/", views.auth_login, name = "login"),
-    path("plantel/", views.plantel_view, name = "plantel"),
     path("loja/", views.loja, name = "loja"),
     path("bilhetes/", views.bilhetes, name = "bilhetes"),
     path("noticias/", views.ultimas_quatro, name = "noticias"),
     path("clube/", views.clube, name = "clube"),
     path("socio/", views.socio, name = "socio"),
+    path("plantel/", views.plantel, name="plantel"),
     path("registar/", views.criar_utilizador, name="registar"),
     path("criarnoticia/", views.criar_noticia, name="criar_noticia"),
+    path("criarproduto/", views.criar_produto, name="criar-produto"),
     path("criarjogo/", views.criar_jogo, name="criar_jogo"),
     path("logout/", views.logout, name="logout")
 ]
